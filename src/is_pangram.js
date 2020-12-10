16c58c0c42338aac42f942796cacbda5de736056
Madeline <madelign@gmail.com>
2020-12-10 18:05:51 -0500

    practice writing tests

--- src/is_pangram.js
+++ src/is_pangram.js
@@ -1,5 +1,27 @@
-const isPangram = function(text) {
+const ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
 
+const isPangram = function(text) {
+	const letterCount = {};
+	
+	letters = text.split('');
+	letters.forEach((letter) => {
+		// if letter is in there, add 1
+		if (letterCount[letter]) {
+			letterCount[letter] += 1;
+		}
+		// if letter is not in there, put it in
+		else {
+			letterCount[letter] = 1;
+		}
+});
+
+
+for (const letter of ALPHABET) {
+	if (!letterCount[letter]) {
+		return false;
+	}
+	return true;
 };
+
 
 module.exports = isPangram;
--- test/is_pangram.test.js
+++ test/is_pangram.test.js
@@ -1,6 +1,8 @@
 const isPangram = require('../src/is_pangram');
 
 describe('isPangram()', () => {
+  // describe('isPangram()', function () {
+    // test function exists
   test('isPangram() is defined', () => {
     expect(isPangram).toBeDefined();
   });
@@ -10,34 +12,44 @@ describe('isPangram()', () => {
     const text = 'the quick brown fox jumps over the lazy dog';
 
     // Act
+    const answer = isPangram(text);
 
     // Assert
+    expect(answer).toEqual(true);
   });
 
   test('works with "abcdefghijklmnopqrstuvwxyz"', () => {
     // Arrange
 
     // Act
+    const answer = isPangram(text);
 
     // Assert
+    expect(answer).toEqual(true);
 
   });
 
   test("missing character 'x'", () => {
     // Arrange
+    const text = 'the quick brown fo jumps over the lazy dog';
 
     // Act
+    const answer = isPangram(text);
 
     // Assert
+    expect(answer).toEqual(false);
 
   });
 
   test('empty sentence', () => {
     // Arrange
+    const text = '';
 
     // Act
+    const answer = isPangram(text);
 
     // Assert
+    expect(answer).toEqual(false);
 
   });
 
@@ -52,10 +64,13 @@ describe('isPangram()', () => {
 
   test('pangram with numbers', () => {
     // Arrange
+    const text = '123456789';
 
     // Act
+    const answer = isPangram(text);
 
     // Assert
+    expect(answer).toEqual(false);
 
   });
 
